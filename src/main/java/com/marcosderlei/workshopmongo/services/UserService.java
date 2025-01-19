cd78f3b4654f97474a7c0d755f03c21eac4cbcfc
Marcos Derlei Meyer <marcos.derlei85@gmail.com>
2025-01-19 11:43:48 -0300

    User insertion with POST

--- src/main/java/com/marcosderlei/workshopmongo/services/UserService.java
+++ src/main/java/com/marcosderlei/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcosderlei.workshopmongo.domain.User;
+import com.marcosderlei.workshopmongo.dto.UserDTO;
 import com.marcosderlei.workshopmongo.repository.UserRepository;
 import com.marcosderlei.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -18,8 +19,16 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	 public User findById(String id) {
-	        return repo.findById(id)
-	                   .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+	public User findById(String id) {
+		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
