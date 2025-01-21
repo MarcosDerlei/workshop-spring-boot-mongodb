7120f02a60e16e57a9be48d72131779ba7520a17
Marcos Derlei Meyer <marcos.derlei85@gmail.com>
2025-01-21 19:25:43 -0300

    User Update with Put

--- src/main/java/com/marcosderlei/workshopmongo/services/UserService.java
+++ src/main/java/com/marcosderlei/workshopmongo/services/UserService.java
@@ -32,7 +32,19 @@ public class UserService {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = repo.findById(obj.getId()).orElseThrow(() -> new RuntimeException("User not found")); // Optional
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
